f05b69ff35519c35eec1fd5cea4a93f7426c0cf8
Bipin kumar <brgaya9431@gmail.com>
2024-07-07 15:20:27 +0530

    from vs code

---
+++ App.js
@@ -0,0 +1,54 @@
+
+import './App.css';
+
+import React, {useState } from 'react'
+import NavBar from './Components/NavBar';
+import News from './Components/News';
+// import NewsItem from './Components/NewsItem';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
+function App() {
+
+  const [mode, setMode] = useState('light');
+
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#232931';
+      document.body.style.color='white';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      document.body.style.color='#232931';
+    }
+  }
+
+    return (
+      <div>
+        <Router basename="/">
+          <NavBar mode={mode} toggleMode={toggleMode}/>
+           <Routes>
+        
+             <Route exact path="/" element={<News mode={mode} key="general" pageSize={20} country="in" category="general"/>} ></Route>
+             <Route exact path="/business" element={<News mode={mode} key="business" pageSize={20} country="in" category="business"/>} ></Route>
+             <Route exact path="/entertainment" element={<News mode={mode} key="entertainment" pageSize={20} country="in" category="entertainment"/>} ></Route>
+             <Route exact path="/general" element={<News mode={mode} key="general" pageSize={20} country="in" category="general"/>} ></Route>
+             <Route exact path="/health" element={<News mode={mode} key="health" pageSize={20} country="in" category="health"/>} ></Route>
+             <Route exact path="/science" element={<News mode={mode} key="science" pageSize={20} country="in" category="science"/>} ></Route>
+             <Route exact path="/sports" element={<News mode={mode} key="sports" pageSize={20} country="in" category="sports"/>} ></Route>
+             <Route exact path="/technology" element={<News mode={mode} key="technology" pageSize={20} country="in" category="technology"/>} ></Route>
+
+        </Routes>
+        </Router>
+
+      </div>
+    )
+  }
+export default App
---
+++ Components/News.js
@@ -0,0 +1,142 @@
+import React, { Component } from 'react'
+// import { Link } from 'react-router-dom';
+
+import NewsItem from './NewsItem'
+
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
+
+
+
+export class News extends Component {
+
+   static defaultProps ={
+         country : 'in',
+         pageSize: 8,
+         category: 'general'
+   }
+
+   static propTypes = {
+         country : PropTypes.string,
+         pageSize: PropTypes.number,
+         category: PropTypes.string
+   }
+
+   capital =(string)=>{
+      return string.charAt(0).toUpperCase() + string.slice(1);
+   }
+
+   constructor(props){
+      super(props);
+      this.state={
+         articles: [],
+         loading : false,
+         page : 1,
+         totalResults: 0,
+          mode: 'light'
+
+      }
+      document.title = `${this.capital(this.props.category)} - NewMonkey`;
+      this.toggleMode = this.toggleMode.bind(this);
+
+   }
+
+   toggleMode() {
+      this.setState(prevState => ({
+        mode: prevState.mode === 'light' ? 'dark' : 'light'
+        
+      }));
+    }
+
+   async componentDidMount(){
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9cb1de8c9fa7477d934edbaf5fc368cf&page=1&pageSize=${this.props.pageSize}`;
+      this.setState({loading:true});
+      let data =await fetch(url);
+      let parsedData= await data.json();
+      console.log(parsedData);
+      this.setState({articles: parsedData.articles,
+         totalResults:parsedData.totalResults,
+      loading: false});
+   }
+
+   handlePreClick=async ()=>{
+      console.log("pre");
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9cb1de8c9fa7477d934edbaf5fc368cf&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+      let data =await fetch(url);
+      let parsedData= await data.json();
+      console.log(parsedData);
+      this.setState({
+         page: this.state.page-1,
+         articles: parsedData.articles
+
+      })
+
+   }
+   handleNextClick=async ()=>{
+      console.log("next");
+      if(!(this.state.page +1 > Math.ceil(this.state.totalResults/5))){
+     
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9cb1de8c9fa7477d934edbaf5fc368cf&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+
+      this.setState({loading:true});
+      let data =await fetch(url);
+      let parsedData= await data.json();
+      console.log(parsedData);
+      this.setState({
+         page: this.state.page+1,
+         articles: parsedData.articles,
+         loading:false
+      })
+   }
+
+   }
+
+   fetchMoreData =async () => {
+      this.setState({page: this.state.page+1})
+      const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9cb1de8c9fa7477d934edbaf5fc368cf&page=1&pageSize=${this.props.pageSize}`;
+
+      let data =await fetch(url);
+      let parsedData= await data.json();
+      console.log(parsedData);
+      this.setState({articles: this.state.articles.concat(parsedData.articles),
+         totalResults:parsedData.totalResults,
+      loading: false})
+    };
+
+  render() {
+   const { mode } = this.state;
+    return (
+      <>
+         <h1 className={`text-center  bg-${!mode}`} >NewsMonkey - {this.capital(this.props.category)} Top headLines </h1>
+         {this.state.loading && <Spinner/>};
+
+         <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner className="my-3"/>}
+        >
+         <div className={`container container-${mode} bg-${!mode}`}>
+         <div className="row md-3">
+            {this.state.articles.map((element)=>{
+           return <div className="col-md-3"  key={element.url}>
+           <NewsItem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,80):""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} mode = {mode}/>
+           </div>
+  })}
+       
+         </div>
+         </div>
+         
+         </InfiniteScroll>
+        
+         {/* <div className="container d-flex justify-content-between">
+      <button disabled={this.state.page<=1} type="button"  className="btn btn-dark " onClick={this.handlePreClick}> &larr; Previous</button>
+      <button disabled={this.state.page +1 > Math.ceil(this.state.totalResults/5)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+      </div> */}
+      </>
+    )
+  }
+}
+
+export default News
